a6ef4e23f3571bfd1df3a857f44d0e2aba95e315
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-08-03 04:53:21 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -25,9 +25,9 @@ class Dog
 
   def save
     sql = <<-SQL
-    INSERT INTO dogs (name, breed) 
+    INSERT INTO dogs (name, breed)
     VALUES (?, ?)
   SQL
-  DB[:conn].execute(sql, self.name, self.album)
+  DB[:conn].execute(sql, self.name, self.breed)
   end
 end
